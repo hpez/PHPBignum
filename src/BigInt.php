06a04c75c5fe277aefaa830ec7469762abdd7a63
Hassan <hpez58@gmail.com>
2018-12-09 22:17:01 +0330

    Bigint subtract

--- src/BigInt.php
+++ src/BigInt.php
@@ -40,4 +40,31 @@ class BigInt
 
         return $this;
     }
+
+    /**
+     * @param BigInt $subNum
+     * @return BigInt
+     */
+    public function subtract($subNum)
+    {
+        $subNumString = $subNum->number;
+        for ($i = 0; $i < strlen($this->number) - strlen($subNumString); $i++)
+            $subNumString = '0'.$subNumString;
+
+        for ($i = 0; $i < strlen($subNumString) - strlen($this->number); $i++)
+            $this->number = '0'.$this->number;
+
+        $carry = 0;
+        for ($i = strlen($subNumString)-1; $i >= 0; $i--) {
+            $value = (intval($this->number[$i]) - intval($subNumString[$i] - $carry));
+            if ($value < 0) {
+                $carry = 1;
+                $value += 10;
+            } else
+                $carry = 0;
+            $this->number[$i] = $value;
+        }
+
+        return $this;
+    }
 }
