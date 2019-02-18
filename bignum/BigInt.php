b5d444456579daca037c49bae7f2c72189a88f45
alex26raider <alex26raider@libero.it>
2019-02-18 12:39:10 +0100

    edit for hpez#18

--- bignum/BigInt.php
+++ bignum/BigInt.php
@@ -85,7 +85,7 @@ class BigInt extends BigNum
 
         $carry = 0;
         for ($i = strlen($addNumString)-1; $i >= 0; $i--) {
-            $value = (intval($this->number[$i]) + intval($addNumString[$i] + $carry)) % 10;
+            $value = ((int)($this->number[$i]) + (int)($addNumString[$i] + $carry)) % 10;
             $carry = floor((intval($this->number[$i]) + intval($addNumString[$i] + $carry)) / 10);
             $this->number[$i] = $value;
         }
